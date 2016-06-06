fb5f741df803456d5dfe9e3d50968355e5b345e8
alpine9000 <alex.g.brown@gmail.com>
2016-06-06 12:33:12 +1000

    dist improvements

--- makedist.sh
+++ makedist.sh
@@ -7,7 +7,20 @@ fi
 rm -rf BlockySkies
 mkdir BlockySkies
 cp game/bin/game1.adf BlockySkies/BlockySkiesPAL_$1.adf
-cat LEGAL.html | sed 's/<p><a href="index.html">Go back<\/a><\/p>//' | html2text.py --ignore-images > BlockySkies/LEGAL.txt
+cat LEGAL.html | sed 's/<p><a href="index.html">Go back<\/a><\/p>//' | html2text.py --ignore-images > LEGAL.txt
+cp LEGAL.txt BlockySkies
 zip -r downloads/BlockySkiesPAL_$1.zip BlockySkies
 lha a downloads/BlockySkiesPAL_$1.lha BlockySkies
-ls -l downloads/BlockySkiesPAL_$1.???
+
+rm -rf BlockySkies
+mkdir BlockySkies
+cp bin/BlockySkiesDrawer.info BlockySkies.info
+cp bin/BlockySkies BlockySkies
+cp bins/BlockySkies.info BlockySkies
+lha -c downloads/BlockySkiesPAL_HD_$1.lha BlockySkies BlockySkies.info
+rm BlockySkies.info
+rm -rf BlockySkies
+rm -f downloads/BlockySkiesPAL_InstallDisk_$1.adf && /usr/local/amiga/amitools/xdftool.py downloads/BlockySkiesPAL_InstallDisk_$1.adf format "BlockySkies" + write bin/BlockySkies.info + write bin/BlockySkies + write LEGAL.txt
+rm LEGAL.txt
+rm -rf BlockySkies
+ls -l downloads/BlockySkiesPAL_*$1.???
