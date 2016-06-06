5591e1eddd90fff7f3ef54a24f4d2fb82eaf030e
alpine9000 <alex.g.brown@gmail.com>
2016-06-06 10:57:50 +1000

    txt based legal

--- makedist.sh
+++ makedist.sh
@@ -7,13 +7,7 @@ fi
 rm -rf BlockySkies
 mkdir BlockySkies
 cp game/bin/game1.adf BlockySkies/BlockySkiesPAL_$1.adf
-mkdir BlockySkies/styles
-mkdir BlockySkies/styles/fonts
-mkdir BlockySkies/images
-cp styles/blockyskies.css BlockySkies/styles
-cp images/apple-touch-icon.png BlockySkies/images
-cp styles/fonts/Topaz_a1200_v1.0.ttf BlockySkies/styles/fonts
-cat LEGAL.html | sed 's/<p><a href="index.html">Go back<\/a><\/p>//' > BlockySkies/LEGAL.html
+cat LEGAL.html | sed 's/<p><a href="index.html">Go back<\/a><\/p>//' | html2text.py --ignore-images > BlockySkies/LEGAL.txt
 zip -r downloads/BlockySkiesPAL_$1.zip BlockySkies
 lha a downloads/BlockySkiesPAL_$1.lha BlockySkies
 ls -l downloads/BlockySkiesPAL_$1.???
